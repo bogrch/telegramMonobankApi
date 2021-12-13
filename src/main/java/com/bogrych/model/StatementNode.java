a60bf439f890805321631345d598387d009f8997
bohrieievy <yevhenii.bohrieiev@solverlabs.com>
2021-12-13 19:36:42 +0200

    encoded amount***

--- src/main/java/com/bogrych/model/StatementNode.java
+++ src/main/java/com/bogrych/model/StatementNode.java
@@ -1,19 +1,6 @@
 package com.bogrych.model;
 
 public class StatementNode {
-//	 "id": "l4I5YWaTbsBCb_re",
-//		 "time": 1639310881,
-//		 "description": "Від: Владислав Боявець",
-//		 "mcc": 4829,
-//		 "originalMcc": 4829,
-//		 "amount": 1000,
-//		 "operationAmount": 1000,
-//		 "currencyCode": 980,
-//		 "commissionRate": 0,
-//		 "cashbackAmount": 0,
-//		 "balance": 925932,
-//		 "hold": true
-
 
 	private long time;
 	private String description;
@@ -47,7 +34,7 @@ public class StatementNode {
 	public String toString() {
 		return "time=" + time +
 			", description='" + description + '\'' +
-			", amount=" + amount +
+			", amount=***" + // amount +
 			'}';
 	}
 }
